879cac63342d8e3897d53f550e2ee1b64db659c2
Justin CAO <CaoQuangBinh@Gmail.com>
2015-09-09 18:20:19 +0700

    [Add] Export database to SDCard
    https://realm.io/docs/java/latest/#the-default-realm

--- app/src/main/java/com/appconus/realmdemo/MainActivity.java
+++ app/src/main/java/com/appconus/realmdemo/MainActivity.java
@@ -1,10 +1,17 @@
 package com.appconus.realmdemo;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 
 import com.appconus.realmdemo.models.User;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.channels.FileChannel;
+import java.util.Calendar;
 
 import io.realm.Realm;
 
@@ -17,6 +24,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         writeToDb();
         readFromDb();
+        exportDatabase(Realm.DEFAULT_REALM_NAME);
     }
 
     private void writeToDb() {
@@ -24,7 +32,7 @@ public class MainActivity extends ActionBarActivity {
         realm.beginTransaction();
         User user = realm.createObject(User.class);
         user.setSessionId(1);
-        user.setName("BinhCQ");
+        user.setName("BinhCQ" + Calendar.getInstance().getTimeInMillis());
         user.setAge(23);
         realm.commitTransaction();
     }
@@ -33,5 +41,29 @@ public class MainActivity extends ActionBarActivity {
         Realm realm = Realm.getInstance(this);
         User user = realm.where(User.class).findFirst();
         Log.d(TAG, "Got user: " + user.getName());
+    }
+
+    private void exportDatabase(String databaseName) {
+        String internalStoragePath =  getFilesDir().getPath();
+        File externalStorageDirectory = Environment.getExternalStorageDirectory();
+        if (externalStorageDirectory.canWrite()) {
+            File currentDB = new File(internalStoragePath, databaseName);
+            File backupDB = new File(externalStorageDirectory, databaseName);
+            if (currentDB.exists()) {
+                copyDatabaseFileToSDCard(currentDB, backupDB);
+            }
+        }
+    }
+
+    private void copyDatabaseFileToSDCard(File currentDB, File backupDB) {
+        try {
+            FileChannel sourceFileChannel = new FileInputStream(currentDB).getChannel();
+            FileChannel destinationFileChannel = new FileOutputStream(backupDB).getChannel();
+            destinationFileChannel.transferFrom(sourceFileChannel, 0, sourceFileChannel.size());
+            sourceFileChannel.close();
+            destinationFileChannel.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
